2885357559f7cd8dc2e452266e3784c57fd3c20f
sayalij1609 <jadhavsayali489@gmail.com>
2026-04-06 15:39:43 +0530

    Final Updated Portfolio Code for deployement

--- api/index.py
+++ api/index.py
@@ -22,3 +22,6 @@ def contact():
 # 🔥 REQUIRED for Vercel
 def handler(environ, start_response):
     return app(environ, start_response)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
